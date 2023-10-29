45913fc3397b9d9a80c5fb2081aba85ca5d87a15
a1rom <dev@alrom.de>
2023-10-29 15:14:08 +0200

    Add GetWeather service for retrieving current weather

---
+++ app/Services/GetWeather.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+
+class GetWeather
+{
+    private string $location;
+    private string $apiKey;
+    
+    public function __construct(string $location)
+    {
+        $this->location = $location;
+        $this->apiKey = env('WEATHERSTACK_API_KEY');
+    }
+
+    public function run(string $units = 'm') : string|false
+    {
+        $client = new Client();
+        $response = $client->get('http://api.weatherstack.com/current', [
+            'query' => [
+                'access_key' => $this->apiKey,
+                'query' => $this->location,
+                'units' => $units,
+            ],
+        ]);
+        
+        if($response->getStatusCode() == 200) {
+            $content = $response->getBody()->getContents();
+            $data = json_decode($content);
+            
+            if(isset($data['sucess']) && $data['success'] === false) {
+                Log::warning('Weatherstack API error: ' . $data);
+                return false;
+            }
+
+            return $content;
+        }
+
+        return false;
+    }
+}
