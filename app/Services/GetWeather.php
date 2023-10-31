c89e6c42bea5ed3aaacf1810b281110e87f6b716
a1rom <dev@alrom.de>
2023-10-31 10:05:59 +0200

    Refactor GetWeather class to include units in constructor

--- app/Services/GetWeather.php
+++ app/Services/GetWeather.php
@@ -8,22 +8,24 @@ use Illuminate\Support\Facades\Log;
 class GetWeather
 {
     private string $location;
+    private string $units;
     private string $apiKey;
     
-    public function __construct(string $location)
+    public function __construct(string $location, string $units = 'm')
     {
         $this->location = $location;
+        $this->units = $units;
         $this->apiKey = env('WEATHERSTACK_API_KEY');
     }
 
-    public function run(string $units = 'm') : string|false
+    public function run() : string|false
     {
         $client = new Client();
         $response = $client->get('http://api.weatherstack.com/current', [
             'query' => [
                 'access_key' => $this->apiKey,
                 'query' => $this->location,
-                'units' => $units,
+                'units' => $this->units,
             ],
         ]);
         
@@ -31,7 +33,7 @@ class GetWeather
             $content = $response->getBody()->getContents();
             $data = json_decode($content, true);
 
-            if($data['success'] === false) {
+            if(array_key_exists('success', $data) && $data['success'] === false) {
                 Log::warning('Weatherstack API error: ' . json_encode($data));
                 return false;
             }
