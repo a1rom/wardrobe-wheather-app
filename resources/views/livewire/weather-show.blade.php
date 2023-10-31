a954ddfcf4a560142582c7041722464093d44909
a1rom <dev@alrom.de>
2023-10-31 10:34:25 +0200

    Update search input styling and error message display

--- resources/views/livewire/weather-show.blade.php
+++ resources/views/livewire/weather-show.blade.php
@@ -1,6 +1,6 @@
 <div>
     {{-- 🔎 Search --}}
-    <div class="flex justify-center relative">
+    <div class="px-2 sm:px-0 flex justify-center relative">
         <div class="relative w-full sm:w-3/4 md:w-3/4 lg:w-1/2">
             <input 
                 type="text" 
@@ -9,13 +9,13 @@
                 @class([
                     'w-full rounded-md py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 placeholder:text-gray-400', 
                     'border-0 ring-gray-300 focus:ring-teal-600 text-gray-900' => !$errors->has('locationSearch'),
-                    'border-1 border-red-400 rind-red-300 focus:ring-red-300 text-red-600' => $errors->has('locationSearch'),
+                    'border border-red-400 ring-red-300 focus:ring-red-300 text-red-600' => $errors->has('locationSearch'),
                 ])
                 placeholder="Enter your location"
                 wire:model.defer="locationSearch"
             >
             @if($errors->has('locationSearch'))
-                <p class="absolute left-0 mt-2 pl-3 sm:pl-0 text-sm text-red-600">
+                <p class="absolute left-0 mt-2 px-2 text-sm text-red-600">
                     {{ $errors->first('locationSearch') }}  
                 </p>
             @endif
@@ -23,7 +23,7 @@
         
         <button 
             type="button" 
-            class="ml-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-600"
+            class="ml-2 inline-flex justify-center py-2 px-4 border-0 border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-600"
             wire:click="search"
         > 
             Search 
